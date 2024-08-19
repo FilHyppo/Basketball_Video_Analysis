2cdf89524029e0a51184f2b8eb7ca39f2bdd8c3a
LorenzoCorradi <82643787+LorenzoCorradi@users.noreply.github.com>
2024-08-19 22:34:53 +0200

    update requirements and cursor

--- main.py
+++ main.py
@@ -3,24 +3,27 @@ from utils.rim_detection import RimDetector
 import cv2
 
 
-# detector = BasketballHoopDetector(
-#     video_path='input_videos/example.mp4',
-#     output_path_color='outputs/example.avi',
-#     hoop_regions=hoop_regions,
-#     output_path_gray='outputs/example_gray.avi',  # Set to None if you don't want a gray video
-#     verbose=True
-# )
-# detector.run()
-
 video_capture = cv2.VideoCapture('input_videos/example.mp4')
 output_video=cv2.VideoWriter('outputs/example.avi', cv2.VideoWriter_fourcc(*'XVID'), video_capture.get(cv2.CAP_PROP_FPS), 
                                             (int(video_capture.get(3)), int(video_capture.get(4))))
-
 _, base_frame = video_capture.read()
-
-
 rim_detector = RimDetector(model_path='models/rim-detection/yolo_v5(my-dataset).pt')
 hoop_regions= rim_detector.detect_rims(base_frame, verbose=True)
+
+detector = BasketballScoreDetector(
+    video_path='input_videos/example.mp4',
+    output_path_color='outputs/example.avi',
+    hoop_regions=hoop_regions,
+    output_path_gray='outputs/example_gray.avi',  # Set to None if you don't want a gray video
+    verbose=True
+)
+detector.run()
+
+
+
+
+
+
 
 for h in range(len(hoop_regions)):      #change  hoops regions for more robust scoring
     height = hoop_regions[h][1][1] - hoop_regions[h][0][1]
